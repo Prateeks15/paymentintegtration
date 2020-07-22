886aab01cdb41bd96ab5f73c53ed4119e24aa1dc
Prateek Singh <prateekchadha15@gmail.com>
2020-07-22 13:54:07 +0530

    Add files via upload

---
+++ config/config.global.js
@@ -0,0 +1,6 @@
+var config = module.exports = {};
+
+config.MERCHANT_ID = '359907159372912';
+config.ACCESS_TOKEN = 'AB8A2BC70A54BB97AEDBD402A696D21A';
+config.ENC_KEY = 'D4CF241AB5FCF6A6DF7577D871E0F28B';
+config.IS_LIVE = false; //Make this false for testing environment
